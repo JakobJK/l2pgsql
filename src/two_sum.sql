29c89671736405f5070958e5cf658cedc00595e2
Jakob Kousholt <jakobjk@gmail.com>
2022-03-02 16:32:30 -0500

    linting

--- src/two_sum.sql
+++ src/two_sum.sql
@@ -3,14 +3,12 @@
 create or replace function two_sum(nums int[], target int) returns int[] as $$ 
     declare result int[]; 
     begin
-    drop table if exists two_sum_table;
-    create temporary table two_sum_table(
-        idx int, 
-        val int
-        );
+        drop table if exists two_sum_table;
+        create temporary table two_sum_table(idx int, val int);
         for idx in 1..array_length(nums, 1)
         loop 
-            insert into two_sum_table (idx, val) values (idx - 1, nums[idx]);
+            insert into two_sum_table (idx, val) 
+            values (idx - 1, nums[idx]);
         end loop;
         select ARRAY[a.idx, b.idx] 
             from two_sum_table a 
